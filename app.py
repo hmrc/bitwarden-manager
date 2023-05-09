fc157a74dff9d53037753032bacf5d2d643aed6a
baarkerlounger <db@slothlife.xyz>
2023-05-09 15:17:15 +0100

    OS Command Injection vulnerability

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # import boto3
 # from botocore.exceptions import ClientError
+import subprocess
 
 
 def handler(event, context):
-    return None
-    # print("event: {}".format(event))
+    domain = input("Enter the Domain: ")
+    subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
