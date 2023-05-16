05f1cd986107a2d60bae342925a2d6b5d6f3474c
cob16 <public+github@cormacbrady.info>
2023-05-16 11:05:47 +0100

    add get_bitwarden_client_id and get_bitwarden_client_secret functions

--- bitwarden_manager/bitwarden_manager.py
+++ bitwarden_manager/bitwarden_manager.py
@@ -8,15 +8,21 @@ class BitwardenManager:
     def __init__(self) -> None:
         self._secretsmanager = AwsSecretsManagerClient(secretsmanager_client=boto3.client("secretsmanager"))
 
-    def get_ldap_username(self) -> str:
-        return self._secretsmanager.get_secret_value("/bitwarden/ldap-username")
-
-    def get_ldap_password(self) -> str:
-        return self._secretsmanager.get_secret_value("/bitwarden/ldap-password")
-
     def run(self) -> None:
         logger = get_bitwarden_logger()
         logger.info(f"retrieved ldap creds with username {self.get_ldap_username()}")
 
         # test log line please remove once we have some real things to do
         logger.info("testing CLIENT_ID organization.KPL8P83fWXAvYvNYcbNWAKAcdNmn4Ssgne7w")
+
+    def get_bitwarden_client_id(self) -> str:
+        return self._secretsmanager.get_secret_value("/bitwarden/api-client-id")
+
+    def get_bitwarden_client_secret(self) -> str:
+        return self._secretsmanager.get_secret_value("/bitwarden/api-client-secret")
+
+    def get_ldap_username(self) -> str:
+        return self._secretsmanager.get_secret_value("/bitwarden/ldap-username")
+
+    def get_ldap_password(self) -> str:
+        return self._secretsmanager.get_secret_value("/bitwarden/ldap-password")
--- tests/bitwarden_manager/test_bitwarden_manager.py
+++ tests/bitwarden_manager/test_bitwarden_manager.py
@@ -5,7 +5,7 @@ from bitwarden_manager.bitwarden_manager import BitwardenManager
 
 
 @mock.patch("boto3.client")
-def test_get_credentials(mock_secretsmanager: Mock) -> None:
+def test_get_ldap_credentials(mock_secretsmanager: Mock) -> None:
     get_secret_value = Mock(return_value={"SecretString": "secret"})
     mock_secretsmanager.return_value = Mock(get_secret_value=get_secret_value)
     manager = BitwardenManager()
@@ -13,6 +13,27 @@ def test_get_credentials(mock_secretsmanager: Mock) -> None:
     assert manager.get_ldap_username() == "secret"
     assert manager.get_ldap_password() == "secret"
 
+    assert get_secret_value.call_count == 2
     get_secret_value.assert_has_calls(
         [call(SecretId="/bitwarden/ldap-username"), call(SecretId="/bitwarden/ldap-password")]
+    )
+
+
+@mock.patch("boto3.client")
+def test_get_bitwarden_creds(mock_secretsmanager: Mock) -> None:
+    get_secret_value = Mock(return_value={"SecretString": "secret"})
+    mock_secretsmanager.return_value = Mock(get_secret_value=get_secret_value)
+    manager = BitwardenManager()
+
+    assert manager.get_bitwarden_client_id() == "secret"
+    assert get_secret_value.call_count == 1
+    get_secret_value.assert_has_calls([call(SecretId="/bitwarden/api-client-id")])
+
+    assert manager.get_bitwarden_client_secret() == "secret"
+    assert get_secret_value.call_count == 2
+    get_secret_value.assert_has_calls(
+        [
+            call(SecretId="/bitwarden/api-client-id"),
+            call(SecretId="/bitwarden/api-client-secret"),
+        ]
     )
