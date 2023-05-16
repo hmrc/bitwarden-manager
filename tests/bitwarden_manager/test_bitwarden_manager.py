23f889844871a9b447ae8ac3442403b51d897d1f
cob16 <public+github@cormacbrady.info>
2023-05-16 11:45:22 +0100

    remove duplicate test

--- tests/bitwarden_manager/test_bitwarden_manager.py
+++ tests/bitwarden_manager/test_bitwarden_manager.py
@@ -5,21 +5,6 @@ from bitwarden_manager.bitwarden_manager import BitwardenManager
 
 
 @mock.patch("boto3.client")
-def test_run(mock_secretsmanager: Mock) -> None:
-    get_secret_value = Mock(return_value={"SecretString": "secret"})
-    mock_secretsmanager.return_value = Mock(get_secret_value=get_secret_value)
-    manager = BitwardenManager()
-
-    assert manager._get_ldap_username() == "secret"
-    assert manager._get_ldap_password() == "secret"
-
-    assert get_secret_value.call_count == 2
-    get_secret_value.assert_has_calls(
-        [call(SecretId="/bitwarden/ldap-username"), call(SecretId="/bitwarden/ldap-password")]
-    )
-
-
-@mock.patch("boto3.client")
 def test_get_ldap_credentials(mock_secretsmanager: Mock) -> None:
     get_secret_value = Mock(return_value={"SecretString": "secret"})
     mock_secretsmanager.return_value = Mock(get_secret_value=get_secret_value)
