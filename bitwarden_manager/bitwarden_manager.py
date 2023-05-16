b49c890792f95bee31c6d3e644c8cbb4354c8ba1
cob16 <public+github@cormacbrady.info>
2023-05-16 12:42:37 +0100

    handle user that has already been invited

--- bitwarden_manager/bitwarden_manager.py
+++ bitwarden_manager/bitwarden_manager.py
@@ -10,24 +10,25 @@ from bitwarden_manager.redacting_formatter import get_bitwarden_logger
 
 class BitwardenManager:
     def __init__(self) -> None:
+        self.__logger = get_bitwarden_logger()
         self._secretsmanager = AwsSecretsManagerClient(secretsmanager_client=boto3.client("secretsmanager"))
 
     def run(self, event: Dict[str, Any]) -> None:
-        logger = get_bitwarden_logger()
-
-        logger.info(f"retrieved ldap creds with username {self._get_ldap_username()}")
+        self.__logger.info(f"retrieved ldap creds with username {self._get_ldap_username()}")
 
         event_name = event["event_name"]
         match event_name:
             case "new_user":
-                logger.debug("handling event with OnboardUser")
+                self.__logger.debug("handling event with OnboardUser")
                 OnboardUser(bitwarden_api=self._get_bitwarden_public_api()).run(event=event)
             case _:
-                logger.info(f"ignoring unknown event '{event_name}'")
+                self.__logger.info(f"ignoring unknown event '{event_name}'")
 
     def _get_bitwarden_public_api(self) -> BitwardenPublicApi:
         return BitwardenPublicApi(
-            client_id=self._get_bitwarden_client_id(), client_secret=self._get_bitwarden_client_secret()
+            logger=self.__logger,
+            client_id=self._get_bitwarden_client_id(),
+            client_secret=self._get_bitwarden_client_secret(),
         )
 
     def _get_bitwarden_client_id(self) -> str:
--- tests/bitwarden_manager/clients/test_bitwarden_public_api.py
+++ tests/bitwarden_manager/clients/test_bitwarden_public_api.py
@@ -1,5 +1,8 @@
+import logging
+
 import pytest
 import responses
+from _pytest.logging import LogCaptureFixture
 from responses import matchers
 
 from bitwarden_manager.clients.bitwarden_public_api import BitwardenPublicApi
@@ -32,6 +35,7 @@ def test_invite_user() -> None:
         )
 
         client = BitwardenPublicApi(
+            logger=logging.getLogger(),
             client_id="foo",
             client_secret="bar",
         )
@@ -53,12 +57,36 @@ def test_failed_invite() -> None:
         )
 
         client = BitwardenPublicApi(
+            logger=logging.getLogger(),
             client_id="foo",
             client_secret="bar",
         )
 
         with pytest.raises(Exception, match="Failed to invite user"):
             client.invite_user(username="test.user", email="test@example.com")
+
+
+@responses.activate
+def test_handle_already_invited_user(caplog: LogCaptureFixture) -> None:
+    with responses.RequestsMock(assert_all_requests_are_fired=True) as rsps:
+        rsps.add(MOCKED_LOGIN)
+        rsps.add(
+            responses.POST,
+            "https://api.bitwarden.com/public/members",
+            body=b'{"object":"error","message":"This user has already been invited.","errors":null}',
+            status=400,
+        )
+
+        client = BitwardenPublicApi(
+            logger=logging.getLogger(),
+            client_id="foo",
+            client_secret="bar",
+        )
+
+        with caplog.at_level(logging.INFO):
+            client.invite_user(username="test.user", email="test@example.com")
+
+        assert "user already invited ignoring error" in caplog.text
 
 
 @responses.activate
@@ -73,6 +101,7 @@ def test_failed_login() -> None:
         )
 
         client = BitwardenPublicApi(
+            logger=logging.getLogger(),
             client_id="foo",
             client_secret="bar",
         )
