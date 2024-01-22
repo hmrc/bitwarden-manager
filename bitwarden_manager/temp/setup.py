219d995cf1826fc45c65cbb4db3c06a3835f8e38
baarkerlounger <db@slothlife.xyz>
2024-01-22 13:47:52 +0000

    Remove temp scripts

--- bitwarden_manager/temp/setup.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from bitwarden_manager.temp.external_id_updater import CollectionUpdater, GroupUpdater  # pragma: no cover
-
-if __name__ == "__main__":  # pragma: no cover
-    CollectionUpdater().setup()
-    GroupUpdater().setup()
