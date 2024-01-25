c8fc843c3f78feb61ea5105a96a4b8866e98f7f4
baarkerlounger <db@slothlife.xyz>
2024-01-25 17:08:06 +0000

    Stricter typing

--- bitwarden_manager/clients/dynamodb_client.py
+++ bitwarden_manager/clients/dynamodb_client.py
@@ -1,12 +1,14 @@
 import boto3
+
 from botocore.exceptions import BotoCoreError, ClientError
+from typing import Dict, Any
 
 
 class DynamodbClient:
     def __init__(self) -> None:
         self._boto_dynamodb = boto3.resource("dynamodb", region_name="eu-west-2")
 
-    def write_item_to_table(self, table_name: str, item: object) -> None:
+    def write_item_to_table(self, table_name: str, item: Dict[str, Any]) -> None:
         try:
             table = self._boto_dynamodb.Table(table_name)
             table.put_item(Item=item)
