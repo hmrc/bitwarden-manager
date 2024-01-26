11be517f3f12a293ce9630fef4528e913030593f
baarkerlounger <db@slothlife.xyz>
2024-01-26 13:41:57 +0000

    Split out write and delete tests

--- tests/bitwarden_manager/clients/test_dynamodb_client.py
+++ tests/bitwarden_manager/clients/test_dynamodb_client.py
@@ -17,7 +17,7 @@ def create_table_in_local_region(dynamodb: dynamodb.Client, table_name: str) -> 
 
 
 @mock_dynamodb  # type: ignore
-def test_write_item_to_table_and_delete_item_from_table() -> None:
+def test_write_item_to_table() -> None:
     client = DynamodbClient()
 
     table_name = "bitwarden"
@@ -32,6 +32,22 @@ def test_write_item_to_table_and_delete_item_from_table() -> None:
     assert table.scan().get("Count") == 1
     key = {"username": "test.user"}
     assert table.get_item(Key=key)["Item"] == item
+
+
+@mock_dynamodb  # type: ignore
+def test_delete_item_from_table() -> None:
+    client = DynamodbClient()
+
+    table_name = "bitwarden"
+    date = datetime.today().strftime("%Y-%m-%d")
+    item = {"username": "test.user", "date": date}
+
+    dynamodb = boto3.resource("dynamodb", region_name="eu-west-2")
+    create_table_in_local_region(dynamodb, table_name)
+    table = dynamodb.Table(table_name)
+    table.put_item(Item=item)
+    assert table.scan().get("Count") == 1
+    key = {"username": "test.user"}
     client.delete_item_from_table(table_name=table_name, key=key)
     assert table.scan().get("Count") == 0
 
